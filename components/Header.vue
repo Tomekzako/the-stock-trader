9415bc5dcab3f7cf389f0fd6cd06cdfabcc369d0
Tomasz Chyc <tomek.chyc55@gmail.com>
2019-04-30 16:00:24 +0200

    setup

---
+++ components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <el-menu
+    :default-active="activeIndex" 
+    class="el-menu-demo" 
+    mode="horizontal"
+    background-color="#545c64"
+    text-color="#fff"
+    active-text-color="#ffd04b">
+    <el-menu-item index="1">
+      <nuxt-link to="/" class="header_link">Stock Trader</nuxt-link>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <nuxt-link to="/portfolio" class="header_link">Portfolio</nuxt-link>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <nuxt-link to="/stocks" class="header_link">Stocks</nuxt-link>
+    </el-menu-item>
+    <el-submenu index="4">
+      <template slot="title">Workspace</template>
+      <el-menu-item index="4-1">item one</el-menu-item>
+      <el-menu-item index="4-2">item two</el-menu-item>
+      <el-menu-item index="4-3">item three</el-menu-item>
+    </el-submenu>
+  </el-menu>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1'
+      }
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+
+<style>
+    .header_link {
+            text-decoration: none;
+        }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <div>
+      <app-header />
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import appHeader from '~/components/Header.vue'
+
+export default {
+  components: {
+    Logo,
+    appHeader
+  }
+}
+</script>
+
+<style>
+</style>
